75ff35f0eae6a9c634de9f1b227dd5bc767b79b2
Salmaslm13 <1304salma@gmail.com>
2022-09-16 23:12:56 +0700

    ajukan judul dan ganti password

---
+++ app/src/main/java/com/example/isscript/pengajuan_semhas.java
@@ -0,0 +1,14 @@
+package com.example.isscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pengajuan_semhas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengajuan_semhas);
+    }
+}
