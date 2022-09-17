f2de5ba4b7dad3a10d83b172759eb28f2735ddb6
baitulazizahh <101490993+baitulazizahh@users.noreply.github.com>
2022-09-17 16:48:19 +0700

    Tes Homescreen

---
+++ app/src/main/java/com/example/isscript/Act_Login.java
@@ -0,0 +1,14 @@
+package com.example.isscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homescreen);
+    }
+}
