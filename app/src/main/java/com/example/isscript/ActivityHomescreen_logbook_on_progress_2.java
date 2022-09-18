489db3f950505e7c50fc115bd628a5b945a85982
baitulazizahh <101490993+baitulazizahh@users.noreply.github.com>
2022-09-19 06:58:11 +0700

    Layout done

--- app/src/main/java/com/example/isscript/ActivityHomescreen_logbook_on_progress_2.java
+++ app/src/main/java/com/example/isscript/ActivityHomescreen_logbook_on_progress_2.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_Login2 extends AppCompatActivity {
+public class ActivityHomescreen_logbook_on_progress_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_homescreen_berhasil_daftar_ta);
     }
 }
