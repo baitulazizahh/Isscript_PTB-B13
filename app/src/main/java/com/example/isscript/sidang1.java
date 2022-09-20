16264dd1a49ed76be4e1e6dfff2beec3fa4a1dd7
gitaptr <2011523011_gita@student.unand.ac.id>
2022-09-20 22:46:29 +0700

    sidangdanpeserta

---
+++ app/src/main/java/com/example/isscript/sidang1.java
@@ -0,0 +1,14 @@
+package com.example.isscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sidang1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sidang1);
+    }
+}
