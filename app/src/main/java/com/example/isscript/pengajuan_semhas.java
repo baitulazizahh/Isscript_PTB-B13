0350d5d163039710f3cb8911713b94902b324d17
gitaptr <2011523011_gita@student.unand.ac.id>
2022-09-21 22:34:54 +0700

    buttononclicklink

--- app/src/main/java/com/example/isscript/pengajuan_semhas.java
+++ app/src/main/java/com/example/isscript/pengajuan_semhas.java
@@ -2,7 +2,9 @@ package com.example.isscript;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class pengajuan_semhas extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class pengajuan_semhas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengajuan_semhas);
+    }
+
+    public void Ajukan (View view) {
+        Intent intent = new Intent(pengajuan_semhas.this, hs.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/isscript/semhas_menunggu_acc.java
+++ app/src/main/java/com/example/isscript/semhas_menunggu_acc.java
@@ -2,7 +2,9 @@ package com.example.isscript;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class semhas_menunggu_acc extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class semhas_menunggu_acc extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_semhas_menunggu_acc);
+    }
+
+    public void Refresh(View view) {
+        Intent intent = new Intent( semhas_menunggu_acc.this, hs_logbook_proses2.class);
+        startActivity(intent);
     }
 }
