05c9a944eac296ba5d39f78b17025a7099e76284
Salmaslm13 <1304salma@gmail.com>
2022-10-19 23:37:51 +0700

    ...

--- app/src/main/java/com/example/isscript/edit_logbook.java
+++ app/src/main/java/com/example/isscript/edit_logbook.java
@@ -15,7 +15,7 @@ public class edit_logbook extends AppCompatActivity {
     }
 
     public void Simpan (View view) {
-        Intent intent = new Intent(edit_logbook.this, hs_logbook_proses2.class);
+        Intent intent = new Intent(edit_logbook.this, selesai_edit.class);
         startActivity(intent);
     }
 }
