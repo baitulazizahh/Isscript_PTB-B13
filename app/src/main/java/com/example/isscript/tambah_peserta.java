0cdefd5866d99a14f61366940d0880e60d915557
baitulazizahh <101490993+baitulazizahh@users.noreply.github.com>
2022-10-19 11:31:17 +0700

    Adapter fix2

--- app/src/main/java/com/example/isscript/tambah_peserta.java
+++ app/src/main/java/com/example/isscript/tambah_peserta.java
@@ -1,17 +1,30 @@
 package com.example.isscript;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
+import com.example.isscript.adapters.PesertaAdapter;
+import java.util.ArrayList;
+
 
 public class tambah_peserta extends AppCompatActivity {
+    private RecyclerView rvPeserta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_peserta);
-    }
+
+        rvPeserta = findViewById(R.id.rv_listpeserta);
+
+        PesertaAdapter adapter = new PesertaAdapter(getPeserta());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        rvPeserta.setLayoutManager(layoutManager);
+        rvPeserta.setAdapter(adapter);
     public void selesaitambahpeserta(View view) {
         Intent intent = new Intent(tambah_peserta.this, daftar_peserta.class);
         startActivity(intent);
@@ -20,3 +33,18 @@ public class tambah_peserta extends AppCompatActivity {
     public void siapdaftarsemhas(View view) {
     }
 }
+    ublic ArrayList<Peserta> getPeserta() {
+        ArrayList<Peserta>listPeserta = new ArrayList<>();
+
+        listPeserta.add(new Peserta(
+                "Baitul Azizah",
+                "2011521002"
+
+        ));
+        listPeserta.add(new Peserta(
+                "Salma",
+                "2011522002"
+        ));
+
+        return listPeserta;
+    };
