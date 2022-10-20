82178cbe93df2022bfbdfa648a3314def603a1c9
baitulazizahh <101490993+baitulazizahh@users.noreply.github.com>
2022-10-20 07:22:44 +0700

    fix4

--- app/src/main/java/com/example/isscript/daftar_peserta.java
+++ app/src/main/java/com/example/isscript/daftar_peserta.java
@@ -1,19 +1,57 @@
 package com.example.isscript;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
 
+import com.example.isscript.adapters.PesertaAdapter;
+import com.example.isscript.models.Peserta;
+
+import java.util.ArrayList;
 
 
 public class daftar_peserta extends AppCompatActivity {
-
+    private RecyclerView rvPeserta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_peserta);
+//        PesertaAdapter adapter = new PesertaAdapter(getPeserta());
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//
+//        rvPeserta.setLayoutManager(layoutManager);
+//        rvPeserta.setAdapter(adapter);
+
     }
+//    public ArrayList<Peserta> getPeserta() {
+//        ArrayList<Peserta> listPeserta = new ArrayList<>();
+//
+//        listPeserta.add(new Peserta(
+//                "Baitul Azizah",
+//                "2011521002"
+//
+//        ));
+//        listPeserta.add(new Peserta(
+//                "Salma",
+//                "2011522002"
+//        ));
+//        listPeserta.add(new Peserta(
+//                "Baitul Azizah",
+//                "2011521002"
+//
+//        ));
+//        listPeserta.add(new Peserta(
+//                "Salma",
+//                "2011522002"
+//        ));
+//
+//        return listPeserta;
+//    }
+
 
 
 
