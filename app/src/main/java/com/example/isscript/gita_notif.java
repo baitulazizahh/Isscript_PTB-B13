3fe85be3b82b6092a78d11c7e48f7a8d37d08474
gitaptr <2011523011_gita@student.unand.ac.id>
2022-12-08 00:11:42 +0700

    notif

---
+++ app/src/main/java/com/example/isscript/gita_notif.java
@@ -0,0 +1,77 @@
+package com.example.isscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.os.Build;
+import android.view.View;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class start extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "test_kanal";
+    private Button buttonShow;
+    private NotificationManagerCompat notificationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notif_gita);
+
+        //ambil notification
+        notificationManager = NotificationManagerCompat.from(this);
+
+        //buat channel notifikasi
+        createNotificationChannel();
+
+
+        buttonShow = findViewById(R.id.buttonshow);
+        buttonShow.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+
+            // Create an Intent for the activity you want to start
+            Intent resultIntent = new Intent(gita_notif.this, .class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(gita_notif.this);
+            stackBuilder.addNextIntentWithParentStack(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(0,
+                            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+            //buat builder utk buat notif
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(notif_gita.this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_baseline_notification_important_24)
+                    .setContentTitle("Notifikasiku")
+                    .setContentText("Jangan Lupa Makan")
+                    .setContentIntent(resultPendingIntent)
+                    .addAction(R.drawable.ic_baseline_notification_important_24,"Lihat", resultPendingIntent)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+            notificationManager.notify(01,builder.build());
+
+        }
+         });
+    }
+    //chanell
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notifikasi pengingat";
+            String description = "Ini hanya kanal untuk contoh notifikasi";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            notificationManager.createNotificationChannel(channel);
+}
+
+        }
+    }
---
+++ app/src/main/java/com/example/isscript/list_logbook.java
@@ -0,0 +1,14 @@
+package com.example.isscript;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_logbook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_logbook);
+    }
+}
