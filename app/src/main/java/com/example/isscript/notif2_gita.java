5ba8de4c2da679255a0f860d28beec16de36ed85
gitaptr <2011523011_gita@student.unand.ac.id>
2022-12-08 07:55:23 +0700

    notifgitafix

--- app/src/main/java/com/example/isscript/notif2_gita.java
+++ app/src/main/java/com/example/isscript/notif2_gita.java
@@ -16,8 +16,7 @@ import android.os.Handler;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class start extends AppCompatActivity {
-
+public class notif2_gita extends AppCompatActivity {
     private static final String CHANNEL_ID = "test_kanal";
     private Button buttonShow;
     private NotificationManagerCompat notificationManager;
@@ -25,7 +24,7 @@ public class start extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.notif_gita);
+        setContentView(R.layout.activity_notif_gta);
 
         //ambil notification
         notificationManager = NotificationManagerCompat.from(this);
@@ -36,31 +35,31 @@ public class start extends AppCompatActivity {
 
         buttonShow = findViewById(R.id.buttonshow);
         buttonShow.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View view) {
+            @Override
+            public void onClick(View view) {
 
-            // Create an Intent for the activity you want to start
-            Intent resultIntent = new Intent(gita_notif.this, .class);
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(gita_notif.this);
-            stackBuilder.addNextIntentWithParentStack(resultIntent);
-            PendingIntent resultPendingIntent =
-                    stackBuilder.getPendingIntent(0,
-                            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                // Create an Intent for the activity you want to start
+                Intent resultIntent = new Intent(notif2_gita.this,a9_semhas.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(notif2_gita.this);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(0,
+                                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
-            //buat builder utk buat notif
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(notif_gita.this, CHANNEL_ID)
-                    .setSmallIcon(R.drawable.ic_baseline_notification_important_24)
-                    .setContentTitle("Notifikasiku")
-                    .setContentText("Jangan Lupa Makan")
-                    .setContentIntent(resultPendingIntent)
-                    .addAction(R.drawable.ic_baseline_notification_important_24,"Lihat", resultPendingIntent)
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                //buat builder utk buat notif
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(notif2_gita.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_baseline_notification_important_24)
+                        .setContentTitle("Notifikasiku")
+                        .setContentText("Jangan Lupa Makan")
+                        .setContentIntent(resultPendingIntent)
+                        .addAction(R.drawable.ic_baseline_notification_important_24,"Lihat", resultPendingIntent)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
 
-            notificationManager.notify(01,builder.build());
+                notificationManager.notify(01,builder.build());
 
-        }
-         });
+            }
+        });
     }
     //chanell
     private void createNotificationChannel() {
@@ -71,7 +70,7 @@ public class start extends AppCompatActivity {
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
             notificationManager.createNotificationChannel(channel);
-}
-
         }
+
     }
+}
