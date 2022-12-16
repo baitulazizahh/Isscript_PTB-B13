5e0951452f506d8c18766fd79fb5258366af1e76
Salmaslm13 <1304salma@gmail.com>
2022-12-16 13:34:51 +0700

    p12 notif pake firebase doneee

--- app/src/main/java/com/example/isscript/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/isscript/MyFirebaseMessagingService.java
@@ -30,8 +30,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        String tipe = remoteMessage.getData().get("tipe");
+        if (tipe.equals("1"))
+            displayNotification(remoteMessage.getNotification());
+        else
+            displayNotification2(remoteMessage.getNotification());
 
-        displayNotification(remoteMessage.getNotification());
+
     }
 
     private void displayNotification(RemoteMessage.Notification notification) {
@@ -44,14 +49,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             channel.setDescription(description);
             NotificationManagerCompat.from(this).createNotificationChannel(channel);
         }
+        NotificationCompat.Builder builder = null;
 
-
-        // 3. Membuat builder untuk membuat notifikasi
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_notification)
+        //Tipe notif 1
+        Intent resultIntent = new Intent(this, b7_ganti_foto.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_isscript)
                 .setContentTitle(notification.getTitle())
                 .setContentText(notification.getBody())
+                .setContentIntent(resultPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        // 4. Membuat objek notifikasi dan menampilkan notifikasi
+        NotificationManagerCompat.from(this).notify(1304, builder.build());
+
+        //Toast.makeText(notif_salma.this, "Test", Toast.LENGTH_SHORT).show();
+    }
+    private void displayNotification2(RemoteMessage.Notification notification) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Contoh notifikasi";
+            String description = "cuman notif aja";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManagerCompat.from(this).createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = null;
+
+        //Tipe notif 2
+        builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_isscript)
+                .setContentTitle(notification.getTitle())
+                .setContentText(notification.getBody())
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         // 4. Membuat objek notifikasi dan menampilkan notifikasi
